903f2aa4f91ae9acac68440ded435ae82e41ecdf
linuxgray <linuxgraytraining@gmail.com>
2025-05-22 18:13:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+def home():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
