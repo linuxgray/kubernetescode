b5b368e9d4c348ec956457adc2290ccfa75092d0
linuxgray <linuxgraytraining@gmail.com>
2025-05-26 14:23:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Flask in Kubernetes!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+def hello_world():
+    return 'Please subscribe, like, and comment on this video, TY!!!'
